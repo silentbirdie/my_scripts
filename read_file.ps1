27614ca2e02ca0d77fb9ed2a6d539cbb7c0b193a
silentbirdie <silentbirdie@gmail.com>
2020-10-03 21:30:41 -0400

    Add files via upload

---
+++ read_file.ps1
@@ -0,0 +1,16 @@
+﻿#what is the file?
+$n=Read-Host ("enter a filepath")
+
+#test the file
+
+test-path -path "$n"
+
+#show contents of file
+
+if ($?) 
+{ more "$n" }
+
+#say it doesn't exist
+
+else ($?)
+{echo "file doesn't exist"}

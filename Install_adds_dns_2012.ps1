439cd642c74fa0a15d5d943887f09d8e6a0f33e0
silentbirdie <silentbirdie@gmail.com>
2020-10-27 13:32:59 -0400

    Update Install_adds_dns_2012.ps1
    it now works and has been tested

--- Install_adds_dns_2012.ps1
+++ Install_adds_dns_2012.ps1
@@ -1,18 +1,20 @@
 #
 # Windows PowerShell script for AD DS Deployment
 # Admin password will be the same as local administrator
+# Change the Domain name to whatever you want before executing script
 #
-Import-Module ADDSDeployment
-Install-ADDSForest `
--CreateDnsDelegation:$false `
--DatabasePath "C:\Windows\NTDS" `
--DomainMode "Win2012R2" `
--DomainName "chrm" `
--DomainNetbiosName "FOREST" `
--ForestMode "Win2012R2" `
--InstallDns:$true `
--LogPath "C:\Windows\NTDS" `
--NoRebootOnCompletion:$false `
--SysvolPath "C:\Windows\SYSVOL" `
--Force:$true
+$DomainName = Read-Host "what will be the domain name?"
+$DomainNetBiosName = Read-Host "what will be the netbios name"
+$AD_Database_Path = Read-Host "what will be the databasepath, sysvolpath and logpath?
+default is normally C:"
+Import-Module ServerManager
 
+Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools
+Install-WindowsFeature DNS -IncludeAllSubFeature -IncludeManagementTools
+
+Import-Module ADDSDeployment
+Import-Module DnsServer
+
+Install-ADDSForest `
+ -DomainName $DomainName `
+ -DomainNetbiosName $DomainNetBiosName -DomainMode Win2012R2 -ForestMode Win2012R2 -DatabasePath $AD_Database_Path\Windows\NTDS -SysvolPath $AD_Database_Path\Windows\SYSVOL -LogPath $AD_Database_Path\Windows\Logs -Force
